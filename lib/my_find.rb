91e4da7f86e1d01099754893a16f19103383ce85
Maria Balsamo <mia.balsamo@gmail.com>
2017-12-20 22:24:27 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,10 +1,17 @@
 require 'pry'
 
 def my_find(collection)
+  if block_given?
+    
   i = 0
   while i < collection.length
     return collection[i] if yield(collection[i])
     i += 1
   end
   collection
+
+  else
+    nil
+  end  
+  
 end
